4e36588d1c9fe858e9a2760a9ea7be0fc4acb504
ThisIsDisruptor <a_s_bas@mail.ru>
2022-02-14 22:03:03 +0300

    first commit

---
+++ src/main/java/com/example/demo/controller/TaskController.java
@@ -0,0 +1,62 @@
+package com.example.demo.controller;
+
+import com.example.demo.model.Task;
+import com.example.demo.repository.TaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.crossstore.ChangeSetPersister;
+import org.springframework.web.bind.annotation.*;
+
+import javax.management.InstanceNotFoundException;
+import java.util.ArrayList;
+
+@RestController
+public class TaskController {
+    @Autowired
+    private TaskRepository taskRepository;
+
+    @PostMapping("/tasks")
+    public Task create(@RequestBody Task task) {
+        return taskRepository.save(task);
+    }
+
+    @GetMapping("/tasks")
+    public Iterable<Task> readAll() {
+        return taskRepository.findAll();
+    }
+
+    @GetMapping("/tasks/{id}")
+    public Task read(@PathVariable("id") long id) {
+        return taskRepository.findById(id).orElse(null);
+    }
+
+    @PutMapping("/tasks/{id}")
+    public Task update(@PathVariable("id") long id, @RequestBody Task task) throws InstanceNotFoundException {
+        if(!taskRepository.existsById(id)) {
+            throw new InstanceNotFoundException();
+        }
+
+        Task updatedTask = new Task();
+        updatedTask.setId(id);
+        updatedTask.setDate(task.getDate());
+        updatedTask.setDescription(task.getDescription());
+        updatedTask.setDone(task.isDone());
+        return taskRepository.save(updatedTask);
+    }
+
+    @DeleteMapping("/tasks/{id}")
+    public void delete(@PathVariable("id") long id) {
+        taskRepository.deleteById(id);
+    }
+
+    @PatchMapping("/tasks/{id}")
+    public void patchMethod(@PathVariable("id") Long id, @RequestBody Task task) {
+        if (task.isDone()) {
+            taskRepository.markAsDone(id);
+        }
+    }
+
+    @PatchMapping("/tasks/{id}:mark-as-done")
+    public void patchMethod(@PathVariable("id") Long id) {
+        taskRepository.markAsDone(id);
+    }
+}
