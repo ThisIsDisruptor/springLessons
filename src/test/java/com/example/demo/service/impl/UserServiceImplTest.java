ce5eebf53a0820db48845de0346c68607400628d
ThisIsDisruptor <a_s_bas@mail.ru>
2022-09-07 00:15:11 +0300

    unit tests for UserServiceImpl

--- src/test/java/com/example/demo/service/impl/UserServiceImplTest.java
+++ src/test/java/com/example/demo/service/impl/UserServiceImplTest.java
@@ -1,5 +1,6 @@
 package com.example.demo.service.impl;
 
+import com.example.demo.model.Task;
 import com.example.demo.model.User;
 import com.example.demo.repository.UserRepository;
 
@@ -14,10 +15,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.test.context.support.WithMockUser;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 @RunWith(MockitoJUnitRunner.class)
 @SpringBootTest
@@ -53,12 +55,9 @@ public class UserServiceImplTest {
 
     @Test
     @WithMockUser(MOCK_USER_NAME)
-    public void getCurrentUser_whenUserExists_ReturnCorrectUser() {
+    public void getCurrentUser_whenUserExists_returnCorrectUser() {
         //arrange
-        User stubUser = new User();
-        stubUser.setId(1l);
-        stubUser.setLogin(MOCK_USER_NAME);
-        stubUser.setPassword("Qwerty123!");
+        User stubUser = createStubUser();
         Mockito.doReturn(Optional.of(stubUser)).when(userRepository).findByLogin(MOCK_USER_NAME);
 
         //act
@@ -71,9 +70,10 @@ public class UserServiceImplTest {
 
     @Test
     @WithMockUser(MOCK_USER_NAME)
-    public void getCurrentUser_whenUserDoesNotExist_ThrowsException() {
+    public void getCurrentUser_whenUserDoesNotExist_throwsException() {
         //assert
         Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            //act
             User foundedUser = userService.getCurrentUser();
         });
 
@@ -81,6 +81,41 @@ public class UserServiceImplTest {
         String actualMessage = exception.getMessage();
 
         assertTrue(actualMessage.contains(expectedMessage));
+    }
+
+    @Test
+    @WithMockUser(MOCK_USER_NAME)
+    public void getCurrentUserTasks_whenUserExists_returnsListTask() {
+        //arrange
+        User stubUser = createStubUserWithTasks();
+        Mockito.doReturn(Optional.of(stubUser)).when(userRepository).findByLogin(MOCK_USER_NAME);
+
+        //act
+        List<Task> tasks = userService.getCurrentUserTasks();
+
+        //assert
+        assertEquals(stubUser.getTasks(), tasks);
+    }
+
+    private User createStubUser() {
+        User stubUser = new User();
+        stubUser.setId(1l);
+        stubUser.setLogin(MOCK_USER_NAME);
+        stubUser.setPassword("Qwerty123!");
+        return stubUser;
+    }
+
+    private List<Task> createStubTaskList() {
+        Task stubTask = new Task();
+        List<Task> stubTaskList = List.of(stubTask);
+        return stubTaskList;
+    }
+
+    private User createStubUserWithTasks() {
+        User stubUser = createStubUser();
+        List<Task> stubTaskList = createStubTaskList();
+        stubUser.setTasks(stubTaskList);
+        return stubUser;
     }
 
 }
